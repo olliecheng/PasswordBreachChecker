e0d6c8195671354e72c6ffe27d2883281f9c18e0
olliethepikachu <olliethepikachu@icloud.com>
2018-09-11 20:58:01 +0800

    First working code base!
    It seems to work alright. Let's hope, ay?

---
+++ checker.js
@@ -0,0 +1,69 @@
+let currentRequest = false;
+
+function hookPasswordFields() {
+    // $("input:password").on("input", changeListener);
+    $("[type=password]").on("input", changeListener);
+}
+
+function statusNeutral(target) {
+    target.removeClass("passwordCheckerStatusPass passwordCheckerStatusFail");
+}
+
+function statusPass(target) {
+    target.removeClass("passwordCheckerStatusFail");
+    target.addClass("passwordCheckerStatusPass");
+}
+
+function statusFail(target) {
+    target.removeClass("passwordCheckerStatusPass");
+    target.addClass("passwordCheckerStatusFail");
+}
+
+function changeListener(event) {
+    let pswdField = $(event.target),
+        val = pswdField.val();
+    if (!val) {
+        statusNeutral(pswdField);
+    } else {
+        statusNeutral(pswdField);
+        let sha = sha1(val).toUpperCase(),
+            shaPrefix = sha.substring(0, 5),
+            shaSuffix = sha.substring(5);
+        if (currentRequest) currentRequest.abort();
+        currentRequest = $.get(
+            "https://api.pwnedpasswords.com/range/" + shaPrefix,
+            "",
+            (data, textStatus, jqXHR) => {
+                currentRequest = false;
+                if (textStatus !== "success") {
+                    alert("Something went wrong with the Password Breach Checker. Please contact the developer with details.")
+                    return;
+                }
+                let results = data.split("\n");
+                if (results.filter(x => x.split(":")[0] == shaSuffix).length) {
+                    statusFail(pswdField);
+                } else {
+                    statusPass(pswdField);
+                }
+            }
+        );
+    }
+}
+
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+var observer = new MutationObserver(function(mutations, observer) {
+    // fired when a mutation occurs
+    console.log(mutations, observer);
+    hookPasswordFields();
+    // ...
+});
+
+// define what element should be observed by the observer
+// and what types of mutations trigger the callback
+observer.observe(document, {
+  subtree: true,
+  childList: true
+});
+
+hookPasswordFields();
